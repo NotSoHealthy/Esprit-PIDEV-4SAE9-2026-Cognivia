411c4fddab181253ab6720bc4b51e3d0e0fb0ee4
souhaibss <souhaib.touaiti12@gmail.com>
2026-02-12 10:37:49 +0100

    empty project just the hierarchy

---
+++ BackEnd/Microservices/tests-service/src/main/java/com/pidev/testsservice/TestsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pidev.testsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsServiceApplication.class, args);
+    }
+
+}
---
+++ BackEnd/Microservices/tests-service/src/test/java/com/pidev/testsservice/TestsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidev.testsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
