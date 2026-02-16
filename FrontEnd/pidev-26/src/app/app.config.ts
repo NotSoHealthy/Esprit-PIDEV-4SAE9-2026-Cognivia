50f479995d4159f727591bbad927e267e65ad898
NotSoHealthy <aminbenhamouda16@gmail.com>
2026-02-16 14:05:20 +0100

    Started work on app layout and dashboard

--- FrontEnd/pidev-26/src/app/app.config.ts
+++ FrontEnd/pidev-26/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth/auth.interceptor';
+import { provideNzIcons } from 'ng-zorro-antd/icon';
+import { AppstoreOutline, UserOutline, UserAddOutline } from '@ant-design/icons-angular/icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideNzIcons([AppstoreOutline, UserOutline, UserAddOutline]),
   ],
 };
