1a5202ee375e37422c632e89bc6b0e183d356493
NotSoHealthy <aminbenhamouda16@gmail.com>
2026-02-17 21:41:31 +0100

    Enable Eureka for care and monitoring

--- BackEnd/Microservices/care/src/main/java/com/pidev/care/CareApplication.java
+++ BackEnd/Microservices/care/src/main/java/com/pidev/care/CareApplication.java
@@ -2,6 +2,7 @@ package com.pidev.care;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CareApplication {
 
     public static void main(String[] args) {
