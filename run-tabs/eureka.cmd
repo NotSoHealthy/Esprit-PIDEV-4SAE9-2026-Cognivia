baa0648b21c6a209fb1a46871c5b54ec67d717de
NotSoHealthy <aminbenhamouda16@gmail.com>
2026-04-15 20:29:37 +0100

    uncached run tabs

--- run-tabs/eureka.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title eureka
-echo [eureka] cd "C:\Users\elite\Desktop\Work\PIDEV-26\BackEnd\eureka"
-pushd "C:\Users\elite\Desktop\Work\PIDEV-26\BackEnd\eureka" || (echo Failed to cd to "C:\Users\elite\Desktop\Work\PIDEV-26\BackEnd\eureka" & pause & exit /b 1)
-mvn -q spring-boot:run
-echo.
-echo [eureka] stopped. Press any key to close tab.
-popd
-pause >nul
--- run-tabs/frontend.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-title frontend
-echo [frontend] cd "C:\Users\elite\Desktop\Work\PIDEV-26\FrontEnd\pidev-26"
-pushd "C:\Users\elite\Desktop\Work\PIDEV-26\FrontEnd\pidev-26" || (echo Failed to cd to "C:\Users\elite\Desktop\Work\PIDEV-26\FrontEnd\pidev-26" & pause & exit /b 1)
-ng serve
-echo.
-echo [frontend] stopped. Press any key to close tab.
-popd
-pause >nul
