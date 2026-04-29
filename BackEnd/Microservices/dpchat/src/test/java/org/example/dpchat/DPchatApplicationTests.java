8acdb5784dfee874d8a0e1241bd56aac629eaaf0
NotSoHealthy <aminbenhamouda16@gmail.com>
2026-04-29 21:25:37 +0100

    Disabled unecessary test

--- BackEnd/Microservices/dpchat/src/test/java/org/example/dpchat/DPchatApplicationTests.java
+++ BackEnd/Microservices/dpchat/src/test/java/org/example/dpchat/DPchatApplicationTests.java
@@ -1,9 +1,9 @@
 package org.example.dpchat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Disabled
 class DPchatApplicationTests {
 
     @Test
